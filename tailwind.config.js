0224e4cd5e5cca8c0af13b2605b59bb5c5dd9177
roderikmogot <roderikmogot17@gmail.com>
2023-11-25 10:29:53 +0700

    project: added vuetify and tailwindcss config

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
