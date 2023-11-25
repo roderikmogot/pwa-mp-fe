9b4178d2a0e4a41b53d1b7e7c7692af42881a85f
roderikmogot <roderikmogot17@gmail.com>
2023-11-25 15:15:33 +0700

    feat: refactor App.vue to use Vue 3 composition API

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,115 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
+import { onMounted, ref, watch } from "vue";
+import useIndexedDB from "./hooks/useIndexedDb";
+
+import ThickPlus from './svgs/ThickPlus.vue';
+
+const { openDB, addItem, getAllItems } = useIndexedDB();
+
+const dialog = ref(false);
+const formInput = ref({
+  title: "",
+  description: "",
+  imageUrl: "",
+});
+
+const recipes = ref([]);
+
+onMounted(async () => {
+  await openDB()
+    .then(console.log)
+    .catch(console.log);
+
+  await getAllItems().then((items) => {
+    console.log('All items:', items);
+    recipes.value = items;
+  });
+})
+
+const onSubmit = async () => {
+  await addItem(formInput.value.title, formInput.value.description, formInput.value.imageUrl)
+    .then(console.log)
+    .catch(console.log);
+
+  dialog.value = false;
+  formInput.value = {
+    title: "",
+    description: "",
+    imageUrl: "",
+  };
+}
+
+const onFileChange = (event) => {
+  const file = event.target.files[0];
+  const reader = new FileReader();
+
+  reader.onload = (e) => {
+    const imageArrayBuffer = e.target.result;
+    formInput.value.imageUrl = imageArrayBuffer;
+  };
+
+  reader.readAsArrayBuffer(file);
+}
+
+watch(formInput, async () => {
+  await getAllItems().then((items) => {
+    console.log('All items:', items);
+    recipes.value = items;
+  });
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main class="min-h-screen w-full relative">
+    <div class="w-full">
+      <div class="container m-auto">
+        <p class="text-[max(48px,min(5vw,76px))] font-bold">Untitled recipe</p>
+
+        <div v-if="recipes.length > 0">
+          <v-expansion-panels variant="accordion">
+            <v-expansion-panel v-for="recipe in recipes" :key="recipe.id" :title="recipe.id + ' ' + recipe.title">
+              <template v-slot:text>
+                <div class="flex items-center flex-col">
+                  <img class="mr-2" :src="recipe.imageUrl" alt="Recipe Image">
+                  <span>{{ recipe.title }}</span>
+                </div>
+              </template>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </div>
+
+        <v-dialog width="500" v-model="dialog">
+          <template v-slot:activator="{ props }">
+            <v-btn v-bind="props" class="w-full" variant="flat" color="primary">
+              <ThickPlus />
+              Add another recipe
+            </v-btn>
+          </template>
+
+          <template v-slot:default="{ isActive }">
+            <v-card title="Add List" class="font-bold">
+              <div class="p-5 grid gap-y-4">
+                <div>
+                  <h3 class="font-bold text-lg">To Do*</h3>
+                  <v-text-field v-model="formInput.title" required hide-details></v-text-field>
+                </div>
+                <div>
+                  <h3 class="font-bold text-lg">Description</h3>
+                  <v-textarea v-model="formInput.description" required hide-details></v-textarea>
+                </div>
+                <div>
+                  <h3 class="font-bold text-lg">Add Image</h3>
+                  <v-file-input accept="image/*" @change="onFileChange"></v-file-input>
+                </div>
+                <v-btn variant="flat" color="primary" text="Submit" @click="onSubmit"></v-btn>
+              </div>
+
+            </v-card>
+          </template>
+        </v-dialog>
+      </div>
+    </div>
+  </main>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
