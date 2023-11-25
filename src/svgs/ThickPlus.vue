513ee42359111ea95b4ae3b4c08598cfbe434288
roderikmogot <roderikmogot17@gmail.com>
2023-11-25 15:15:20 +0700

    feat: Add ThickPlus.vue component

---
+++ src/svgs/ThickPlus.vue
@@ -0,0 +1,20 @@
+<template>
+    <svg-icon type="mdi" :path="path"></svg-icon>
+</template>
+  
+<script>
+import SvgIcon from '@jamescoyle/vue-icon';
+import { mdiPlusThick } from '@mdi/js';
+
+export default {
+    name: "thick-plus",
+    components: {
+        SvgIcon
+    },
+    data() {
+        return {
+            path: mdiPlusThick,
+        }
+    }
+}
+</script>
